e05fc088b5e1fa6cc6439618c0d3df95975a1fd9
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-29 15:19:15 -0300

    Add product service

---
+++ src/AbpDemo.Application.Contracts/Products/IProductAppService.cs
@@ -0,0 +1,15 @@
+﻿using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace AbpDemo.Products
+{
+    public interface IProductAppService :
+     ICrudAppService< //Defines CRUD methods
+         ProductDto, //Used to show books
+         long, //Primary key of the book entity
+         PagedAndSortedResultRequestDto, //Used for paging/sorting
+         CreateUpdateProductDto> //Used to create/update a book
+    {
+
+    }
+}
---
+++ src/AbpDemo.Application/Products/ProductAppService.cs
@@ -0,0 +1,22 @@
+﻿using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace AbpDemo.Products
+{
+    public class ProductAppService :
+  CrudAppService<
+      Product, //The Product entity
+      ProductDto, //Used to show Products
+      long, //Primary key of the Product entity
+      PagedAndSortedResultRequestDto, //Used for paging/sorting
+      CreateUpdateProductDto>, //Used to create/update a Product
+  IProductAppService//implement the IProductAppService
+    {
+        public ProductAppService(IRepository<Product, long> repository)
+            : base(repository)
+        {
+
+        }
+    }
+}

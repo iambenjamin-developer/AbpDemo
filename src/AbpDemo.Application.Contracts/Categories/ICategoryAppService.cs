e42e1275703325b076a84e42ede468375766e04d
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-29 15:17:51 -0300

    Add category service

---
+++ src/AbpDemo.Application.Contracts/Categories/ICategoryAppService.cs
@@ -0,0 +1,15 @@
+﻿using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace AbpDemo.Categories
+{
+    public interface ICategoryAppService :
+    ICrudAppService< //Defines CRUD methods
+        CategoryDto, //Used to show books
+        long, //Primary key of the book entity
+        PagedAndSortedResultRequestDto, //Used for paging/sorting
+        CreateUpdateCategoryDto> //Used to create/update a book
+    {
+
+    }
+}
---
+++ src/AbpDemo.Application/Categories/CategoryAppService.cs
@@ -0,0 +1,22 @@
+﻿using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace AbpDemo.Categories
+{
+    public class CategoryAppService :
+   CrudAppService<
+       Category, //The Category entity
+       CategoryDto, //Used to show categories
+       long, //Primary key of the Category entity
+       PagedAndSortedResultRequestDto, //Used for paging/sorting
+       CreateUpdateCategoryDto>, //Used to create/update a Category
+   ICategoryAppService//implement the ICategoryAppService
+    {
+        public CategoryAppService(IRepository<Category, long> repository)
+            : base(repository)
+        {
+
+        }
+    }
+}
